e64278ccfd0cb60961e36cff81f677735ef7bdc0
Sronder <sara.ronder@gmail.com>
2022-06-10 21:22:05 -0500

    adding retirement db

--- schema.sql
+++ schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE dept_manager (
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+    PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE salaries (
@@ -37,12 +37,12 @@ CREATE TABLE salaries (
 
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
-    dept_no VARCHAR NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -51,7 +51,5 @@ CREATE TABLE titles (
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, title, from_date)
 );
-
-SELECT * FROM departments;
